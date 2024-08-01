cef3c3c16035f58168586a89ff336d7df9a1e88c
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2024-07-31 20:12:16 -0400

    created simple golang app

---
+++ go-todo/go.mod
@@ -0,0 +1,3 @@
+module go-todo.apps.jpech.dev
+
+go 1.22.0
---
+++ go-todo/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type todo struct {
+	ID   int
+	Text string
+	Done bool
+}
+
+type application struct {
+	store []todo
+}
+
+func (app *application) indexPage(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./index.html")
+
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, app.store)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func (app *application) createTodo(w http.ResponseWriter, r *http.Request) {
+    err := r.ParseForm()
+    if err!=nil{
+        http.Error(w, "Client Error", http.StatusBadRequest)
+        return
+    }
+
+    log.Print(r.PostForm)
+
+    newTodo := todo{
+        ID: 1,
+        Text: r.PostForm.Get("new-todo"),
+        Done: false,
+    }
+
+    app.store = append(app.store, newTodo)
+
+    http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func main() {
+	port := ":80"
+
+	app := &application{
+		store: []todo{
+			{
+				ID:   0,
+				Text: "Code apps",
+				Done: true,
+			},
+			{
+				ID:   1,
+				Text: "Deploy them",
+				Done: false,
+			},
+		},
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", app.indexPage)
+    mux.HandleFunc("POST /new-todo", app.createTodo)
+
+	log.Print("running go server in port ", port)
+	log.Fatal(http.ListenAndServe(port, mux))
+}
